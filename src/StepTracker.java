19ccf0368243ae3160c5874e018ab12090acbcbb
Sergey Tarasov <s.d.tarasov@ya.ru>
2022-03-10 21:16:26 +0300

    Исправленная версия для ревью

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -15,7 +15,7 @@ public class StepTracker {
     // п.2 меню - Напечатать статистику за определённый месяц.
     public void printStats() {
         System.out.println("Номер месяца, за который хотите увидеть статистику и нажмите ENTER");
-        int monthNumberForPrint = scanner.nextInt();
+        int monthNumberForPrint = getUserInput();
         System.out.println("Месяц " + monthNumberForPrint);
         MonthData monthData = monthToDataMap.get(monthNumberForPrint);
         monthData.printData();
@@ -30,31 +30,31 @@ public class StepTracker {
     // п.1 меню - Ввести количество шагов за определённый день.
     public void setDaySteps() {
         System.out.println("Введите номер месяца (1 - январь, 12 - декабрь): ");
-        int monthNumber = scanner.nextInt();
+        int monthNumber = getUserInput();
         while (true) {
             if (monthNumber < 1 || monthNumber > 12) {
                 System.out.println("В году не бывает " + monthNumber + " месяца. Введите число от 1 до 12");
-                monthNumber = scanner.nextInt();
+                monthNumber = getUserInput();
             } else {
                 break;
             }
         }
         System.out.println("Введите число (от 1 до 30): ");
-        int dayNumber = scanner.nextInt();
+        int dayNumber = getUserInput();
         while (true) {
             if (dayNumber < 1 || dayNumber > 30) {
                 System.out.println("Нужно ввести число от 1 до 30");
-                dayNumber = scanner.nextInt();
+                dayNumber = getUserInput();
             } else {
                 break;
             }
         }
         System.out.println("Введите количество пройденных шагов: ");
-        int stepCount = scanner.nextInt();
+        int stepCount = getUserInput();
         while (true) {
             if (stepCount < 0) {
                 System.out.println("Количество шагов не может быть отрицательным. Введите корректное значение.");
-                stepCount = scanner.nextInt();
+                stepCount = getUserInput();
             } else {
                 break;
             }
@@ -69,7 +69,7 @@ public class StepTracker {
         System.out.println("Текущая цель " + currentStepGoal + " шагов.");
         while (true) {
             System.out.println("Введите новую цель в шагах (0 - для возврата в главное меню): ");
-            int userInput = scanner.nextInt();
+            int userInput = getUserInput();
             if (userInput == 0) {
                 break;
             }
@@ -81,5 +81,17 @@ public class StepTracker {
             }
         }
         System.out.println("Ваша цель - " + currentStepGoal + " шагов!");
+    }
+
+    //Отсечение ввода текста
+    public int getUserInput() {
+        while (true) {
+            try {
+                String userInput = scanner.nextLine();
+                return Integer.parseInt(userInput);
+            } catch (NumberFormatException e) {
+                System.out.println("Введите число!");
+            }
+        }
     }
 }
